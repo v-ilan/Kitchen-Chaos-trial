559d30ea0035db8154dbf6ce73e9a7805f5b53c2
v-ilan <ilanvol96@gmail.com>
2026-03-01 14:36:08 +0200

    Update PlateCompleteVisual.cs
    unsubscribe listener

--- Kitchen Chaos/Assets/Scripts/PlateCompleteVisual.cs
+++ Kitchen Chaos/Assets/Scripts/PlateCompleteVisual.cs
@@ -36,4 +36,12 @@ public class PlateCompleteVisual : MonoBehaviour
             }
         }
     }
+
+    private void OnDestroy() 
+    {
+        if (plateKitchenObject != null) 
+        {
+            plateKitchenObject.OnAddIngredient -= PlateKitchenObjectOnAddIngredient;
+        }
+    }
 }
